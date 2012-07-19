0651f1d7d2c36f97fd21e541e8ce052e15af112a
Russell Harrison <rharriso@redhat.com>
2012-07-19 11:21:30 -0400

    define the require_relative method for ruby versions that don't have it already.

--- apply_catalog_with_files.rb
+++ apply_catalog_with_files.rb
@@ -2,6 +2,14 @@
 #
 # Copyright PuppetLabs 2010
 
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative 'trollop'
 require 'pp'
 require 'puppet'
