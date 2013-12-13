66a7b4c3c850b7fd10a303f6cdb52affccc1ae1d
Russell Harrison <rharriso@redhat.com>
2013-12-12 19:10:53 -0500

    Remove the files after the run has completed

--- apply_compiled_catalog_tarball.rb
+++ apply_compiled_catalog_tarball.rb
@@ -4,10 +4,19 @@ nodefile = ARGV[0]
 nodefile =~ /(.*)\.compiled_catalog_with_files.tar.gz/
 nodename = $1
 
-puts nodefile + " " + nodename
-
 system("tar -xvPf #{nodefile}")
 
 modulepath = File.open("#{nodename}.modulepath").readlines.first
 
-system("puppet apply --debug --catalog #{nodename}.catalog.pson --modulepath '#{modulepath}'")
+system("puppet apply --detailed-exitcodes --verbose --write-catalog-summary --catalog #{nodename}.catalog.pson --modulepath '#{modulepath}'")
+
+# Clean up the files down loaded to the file system.
+# Get the list of files from the tarball
+nodefile_files = `tar tzvf backup1.test.rhcloud.com.compiled_catalog_with_files.tar.gz`.split(/\n/).map { |line| line.split[5] }
+
+# Sorting and reversing the array means we'll operate on any directories after
+# their files have already been removed.
+nodefile_files.sort.reverse.each do | file_name |
+  File.delete(file_name) if File.file?(file_name)
+  Dir.delete(file_name) if File.directory?(file_name)
+end
